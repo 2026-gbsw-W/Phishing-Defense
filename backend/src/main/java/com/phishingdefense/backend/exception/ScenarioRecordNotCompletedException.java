68c023bf019325900dc4872e0643de690d4e939a
Void <lollohol755@gmail.com>
2026-07-13 22:19:45 +0900

    feat : 리포트 생성, 조회, 에외 처리

--- backend/src/main/java/com/phishingdefense/backend/exception/ScenarioRecordNotCompletedException.java
+++ backend/src/main/java/com/phishingdefense/backend/exception/ScenarioRecordNotCompletedException.java
@@ -3,6 +3,6 @@ package com.phishingdefense.backend.exception;
 public class ScenarioRecordNotCompletedException extends RuntimeException {
 
     public ScenarioRecordNotCompletedException(Long recordId) {
-        super("완료되지 않은 플레이 기록으로는 미션을 완료할 수 없습니다: " + recordId);
+        super("완료되지 않은 플레이 기록입니다: " + recordId);
     }
 }
